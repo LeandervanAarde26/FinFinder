47077fa478eded88030975da809176ed55bdd9c2
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-02-22 18:46:21 +0200

    Main styles have been setup
    Styles.scss now contain all the colours that will be used in the appliation, headings have been setup and general configuration has been setup. this is all done through variables and will be globally accessible.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './Routes/landing/landing.component';
+import { LoginComponent } from './Routes/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
