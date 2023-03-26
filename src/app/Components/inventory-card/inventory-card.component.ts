40dab77030d05505123c5cc31fe258e9e39d9afa
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-03-27 00:10:29 +0200

    quick update started
    Quick update starting to be implemented

--- src/app/Components/inventory-card/inventory-card.component.ts
+++ src/app/Components/inventory-card/inventory-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 
 
 @Component({
@@ -6,13 +6,36 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
   templateUrl: './inventory-card.component.html',
   styleUrls: ['./inventory-card.component.scss']
 })
-export class InventoryCardComponent {
+export class InventoryCardComponent implements OnInit {
   @Input() name: string;
   @Input() image: string; 
   @Input() quantity: number;
+  @Input() id : string;
   @Output() clickHandler = new EventEmitter<any>();
+  edit: boolean = false;
+  newValue: number 
 
   onClickHandler(e) {
     this.clickHandler.emit(e)
+  }
+
+  changeUpdateState(){
+    this.edit = !this.edit
+  }
+
+  editItem(id){
+    console.log(id)
+  }
+
+  Increment(){
+      this.newValue ++
+  }
+
+  Decrement(){
+    this.newValue --
+  }
+
+  ngOnInit(){
+    this.newValue = this.quantity
   }
 }
