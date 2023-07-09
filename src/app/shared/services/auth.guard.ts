dbc1659f6098d5678bc48882af2277f83f0ba87f
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-07-09 14:23:30 +0200

    update AuthGuard

--- src/app/shared/services/auth.guard.ts
+++ src/app/shared/services/auth.guard.ts
@@ -8,16 +8,13 @@ import { AuthService } from './auth.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService){}
-
-
-
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot):  boolean | UrlTree {
       const user = this.authService.user
       const sessionUser: string = sessionStorage.getItem('user')
 
-      if(!sessionUser || !user  ){
+      if(!sessionUser){
         return this.router.parseUrl("/")
       }
     return true;
